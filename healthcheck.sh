5cfd4a82165b64078f24e5ea27ac7bc9e9e96b90
Vivek Kaushik <Vivekkaushik82@gmail.com>
2023-04-01 23:11:04 +0530

    changes to docker file to add curl and jq and added echo to shell script

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,7 +4,7 @@
 # BROWSER
 # MODULE
 echo "Checking if hub is ready - $HUB_HOST"
-cat healthcheck.sh
+echo "http://"$HUB_HOST":4444/wd/hub/status"
 while [ "$( curl -s http://"$HUB_HOST":4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
 echo "inside loop"
